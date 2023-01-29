ad06e50a90dc99ab96dbd1e29e4f03edbaa54105
Leon <Leon.Engineer@hotmail.com>
2023-01-29 16:49:01 +0800

    Add development/tabledemo page;

---
+++ Masa.Blazor.Pro/Pages/Development/TableDemo.razor.cs
@@ -0,0 +1,30 @@
+﻿namespace Masa.Blazor.Pro.Pages.Development;
+
+public partial class TableDemo : ProCompontentBase
+{
+    protected override void OnInitialized()
+    {
+        MasaBlazor.Breakpoint.OnUpdate += OnPropertyChanged;
+        MasaBlazor.Application.PropertyChanged += OnPropertyChanged;
+    }
+
+    private Task OnPropertyChanged()
+    {
+        if (NavHelper.CurrentUri.EndsWith("development/tabledemo"))
+        {
+            InvokeAsync(StateHasChanged);
+        }
+        return Task.CompletedTask;
+    }
+
+    private void OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        OnPropertyChanged();
+    }
+
+    public void Dispose()
+    {
+        MasaBlazor.Breakpoint.OnUpdate -= OnPropertyChanged;
+        MasaBlazor.Application.PropertyChanged -= OnPropertyChanged;
+    }
+}
